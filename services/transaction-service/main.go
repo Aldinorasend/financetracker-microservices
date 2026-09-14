203fee2c96fcb233aef6a6722404e73da0f6a905
d0ughnuts4u <aldinorasend@gmail.com>
2026-09-14 07:54:35 +0700

    exercise 3: creating post method without database only using array

--- services/transaction-service/main.go
+++ services/transaction-service/main.go
@@ -6,14 +6,35 @@ import (
 	"net/http"
 )
 
+// Defining structure healthResponse to respond to healthcheck request
 type HealthResponse struct {
 	Status  string `json:"status"`
 	Service string `json:"service"`
+	Version string `json:"version"`
+}
+
+// defining structure transaction to respond to transaction request
+type Transaction struct {
+	ID          int     `json:"id"`
+	Type        string  `json:"type"`
+	Amount      float64 `json:"amount"`
+	Category    string  `json:"category"`
+	Description string  `json:"description"`
+}
+
+// Slice for saving transaction
+var transactions []Transaction
+var nextID = 1
+
+type HelloResponse struct {
+	Message string `json:"message"`
 }
 
 func main() {
 	// Creating endpoint and calling handler
 	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/transactions", createTransactionHandler)
 
 	// Make it easier to change port
 	fmt.Println("Server running on http://localhost:8080")
@@ -27,12 +48,55 @@ func main() {
 
 // Output shown in browser
 func healthHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Delivering structured response
 	res := HealthResponse{
 		Status:  "OK",
 		Service: "transaction-service",
+		Version: "1.0.0",
 	}
 
 	w.Header().Set("Content-type", "application/json")
 
 	json.NewEncoder(w).Encode(res)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Delivering structured response
+	res := HelloResponse{
+		Message: "Hello from Financial Tracker",
+	}
+
+	w.Header().Set("Content-type", "application/json")
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func createTransactionHandler(w http.ResponseWriter, r *http.Request) {
+	// Defining variable transaction
+	var transaction Transaction
+
+	// decoding request body
+	err := json.NewDecoder(r.Body).Decode(&transaction)
+
+	// checking error during decoding
+	if err != nil {
+		http.Error(w, "Invalid Req Body", http.StatusBadRequest)
+		return
+	}
+
+	// Creating ID and increasing nextID
+	transaction.ID = nextID
+	nextID++
+
+	// Appending transaction to slice
+	transactions = append(transactions, transaction)
+
+	// Returning the same transaction as response
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	// Encoding and delivering response
+	json.NewEncoder(w).Encode(transaction)
+
 }
